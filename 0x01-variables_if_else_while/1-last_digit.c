834324317665e23dbd13f932a0b7d14648cf33ec
AndrewKamau <andrewgithinji@hotmail.com>
2022-07-01 17:34:11 +0000

    Positive anything is better than negative nothing

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n, lastDigit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastDigit = n % 10;
+	/* your code goes there */
+	printf(lastDigit of %d is\n",);
+	if (n > 5)
+	
+	printf("and is greater than 5\n");{
+
+	else if (n == 0)
+
+	printf("and is zero\n");{
+
+	else if ((n < 6),(
+	return (0);
+}
