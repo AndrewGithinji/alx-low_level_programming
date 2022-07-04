a00fdece32192ee0cceb9e5d047f625960a8f6fa
AndrewKamau <andrewgithinji@hotmail.com>
2022-07-04 17:40:34 +0000

    prints all possible different combinations of two digits.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,7 +18,7 @@ int main(void)
 
 			if (digit1 == 8 && digit2 == 9)
 				continue;
-			
+
 			putchar(',');
 			putchar(' ');
 		}
@@ -27,4 +27,4 @@ int main(void)
 	putchar('\n');
 
 	return (0);
-}	
+}
