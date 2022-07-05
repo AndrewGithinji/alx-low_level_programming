bba76437027fcb52cf7ab8cc32f3a90f11e2a821
AndrewKamau <andrewgithinji@hotmail.com>
2022-07-05 20:05:04 +0000

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*int add - adds two integers and returns the result.
+*@int: displays integer
+* Return: the result.
+*/
+int add(int, int);
+{
+	int Num 1, Num 2, Sum;
+
+	for ((Num 1 + Num 2))
+	{
+	return (Sum);
+	}
+}
+
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,8 @@
+#include "main.h"
+  1 
+  2 /**
+  3 *_islower - checks for lowercase char
+  4 *@c: displays character
+* Return:1 if c is lowercase 0 otherwise
+*/
+
