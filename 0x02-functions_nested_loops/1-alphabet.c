41ddf64a7301da0913d707f65aeb26fdbcae04a3
AndrewKamau <andrewgithinji@hotmail.com>
2022-07-05 13:59:45 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - entry point
+*
+* Return: success is always zero
+*/
+void print_alphabet(void);
+{
+	char x;
+
+	for (x = a; x <= z; a++)
+	{
+	_putchar(x);
+	}	
+	_putchar('\n')
+}
