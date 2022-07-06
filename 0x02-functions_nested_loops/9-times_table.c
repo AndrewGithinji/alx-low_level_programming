374b1d0641539b88ad09e8a63b0a844776b10fe6
AndrewKamau <andrewgithinji@hotmail.com>
2022-07-06 16:45:16 +0000

    Multiple mains

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,7 +2,7 @@
 
 /**
  * times_table - a function that prints the 9 times table, starting with 0.
- *
+	*
  * Return: Always 0.
  */
 
@@ -11,17 +11,17 @@ void times_table(void)
 	int x, y, product;
 
 	for (x = 0; x <= 9; x++)
-	{
-		for (y = 0; y <= 9; y++)
+		{
+	 	for (y = 0; y <= 9; y++)
 		{
 			product = x * y;
-			if (y != 0)
-			{
-				_putchar(',');
+	 		if (y != 0)
+	 		{
+	  			_putchar(',');
 				_putchar(' ');
 			}
 			if (y == 0)
-			{
+		{
 				_putchar('0');
 			}
 			else if (product >= 10)
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 alx low level programming
+
+
+Debugging is the process of finding and fixing errors in software that prevents it from running correctly.
