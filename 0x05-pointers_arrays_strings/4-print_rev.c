73c4639f72a8297fbd2f05f7ef7693d932d8d190
AndrewKamau <andrewgithinji@hotmail.com>
2022-07-11 16:11:31 +0000

    3-puts.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,16 +2,16 @@
 #include <string.h>
 
 /**
-* _puts -  a function that prints a string
+* _puts - prints a string
 * @str: string
-* Return: zero
+* Return: 0
 */
 void _puts(char *str)
 {
 	int len, i;
 
 	len = strlen(str);
-	for (i = 0; i < len; i++)
+	for (i = 0; i < len; i--)
 	{
 	_putchar(str[i]);
 	}
