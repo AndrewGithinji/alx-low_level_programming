7ea16c661f6ac28e06e741d160fc461899450189
AndrewKamau <andrewgithinji@hotmail.com>
2022-07-14 18:33:29 +0000

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+/**
+* reverse_array - reverses contents of an array
+* @a: first pointer
+* @n: second parameter
+* Return: 0
+*/
+void reverse_array(int *a, int n)
+{
+	int temp[n];
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		temp[n - 1 - i] = a[i];
+	}
+	for (i = 0; i < n; i++)
+	{
+		a[i] = temp[i];
+	}
+}
