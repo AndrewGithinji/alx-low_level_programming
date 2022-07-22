8dcd822f0bcafb81374ecbb60fe6409ef36ec5fb
AndrewGithinji <andrewgithinji@hotmail.com>
2022-07-22 17:03:47 +0000

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,39 +1,20 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * main - coins to make change for an amount of money
- * @argc: argument counter
- * @argv: array of pointer to strings
- *
- * Return: 0 if no errors, else
- */
+* main - program that prints its name
+* @argc: counts nuber of arguments
+* @argv: the program name
+* Return: zero is success
+*/
+
 int main(int argc, char *argv[])
 {
-	int a, n = 0, i, t;
-	int c[5] = {25, 10, 5, 2, 1};
+	int i;
 
-	if (argc != 2)
+	for (i = 0; i < argc; i++)
 	{
-	puts("Error");
-	return (1);
+	printf("%s\n", argv[i]);
 	}
-	a = atoi(argv[1]);
-	if (a <= 0)
-	{
-	puts("0");
-	return (1);
-	}
-	else
-	{
-	for (i = 0; i < 5; i++)
-	{
-	t = a / c[i];
-	a -= t * c[i];
-	n += t;
-	if (a == 0)
-	break;
-	}
-	}
-	printf("%d\n", n);
 	return (0);
-
+}
