9504e47a3ef139a959d656d2c9f2fe71da8de5d8
AndrewGithinji <andrewgithinji@hotmail.com>
2022-08-16 17:07:35 +0000

    7. Get node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,7 +3,7 @@
 
 /**
 *get_nodeint_at_index - find node
-*@head: pointer to 
+*@head: pointer to
 *@index: index
 *Return: head
 */
@@ -13,7 +13,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (head == NULL)
 	return (NULL);
-	
+
 	for (i = 0; i < index; i++)
 	{
 	head = head->next;
